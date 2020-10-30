ecfe24b98f9cc7ac3e8ce0f9a1116e27fff81c8f
phwt.smwt <phwt.smwt@gmail.com>
2020-10-30 21:31:17 +0700

    Revert "Test calendar"
    This reverts commit 39074282919ecba9e77b02c50b10c18205421a91.

--- App.js
+++ App.js
@@ -1,23 +1,10 @@
-import { StatusBar } from "expo-status-bar";
+// import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import Calendar from "./views/calendar/Calendar";
+// import { StyleSheet, Text, View } from "react-native";
+import Navigator from "./modules/Navigator";
 
-export default function App() {
-  return (
-    <View style={{ flex: 1 }}>
-      <Text>Test</Text>
-      <StatusBar style="auto" />
-      <Calendar />
-    </View>
-  );
-}
+const App = () => {
+  return <Navigator />;
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+export default App;
