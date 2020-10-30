cafab4b6d34627584b48a09ac1309389fc1c098a
Tummasorn Tantiyapinun <42957781+tangtms@users.noreply.github.com>
2020-10-30 21:37:36 +0700

    Update Calendar.js

--- views/calendar/Calendar.js
+++ views/calendar/Calendar.js
@@ -1,8 +1,60 @@
-import React from "react";
-import { Text } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Agenda } from "react-native-calendars";
 
-const Calendar = () => {
-  return <Text>Calendar</Text>;
+const timeToString = (time) => {
+  const date = new Date(time);
+  return date.toISOString().split("T")[0];
 };
 
+const Calendar = () => {
+  const [items, setItems] = useState({});
+  const loadItems = (day) => {
+    setTimeout(() => {
+      for (let i = -15; i < 85; i++) {
+        const time = day.timestamp + i * 24 * 60 * 60 * 1000;
+        const strTime = timeToString(time);
+        if (!items[strTime]) {
+          items[strTime] = [];
+          const numItems = Math.floor(Math.random() * 3 + 1);
+          for (let j = 0; j < numItems; j++) {
+            items[strTime].push({
+              name: "Item for " + strTime + " #" + j,
+              height: Math.max(50, Math.floor(Math.random() * 150)),
+            });
+          }
+        }
+      }
+      const newItems = {};
+      Object.keys(items).forEach((key) => {
+        newItems[key] = items[key];
+      });
+      setItems(newItems);
+    }, 1000);
+  };
+
+  return (
+    <View style={{ flex: 1 }}>
+      <Agenda
+        // testID={testIDs.agenda.CONTAINER}
+        items={items}
+        loadItemsForMonth={loadItems}
+        selected={"2020-10-30"}
+        // renderItem={this.renderItem.bind(this)}
+        // renderEmptyDate={this.renderEmptyDate.bind(this)}
+        // rowHasChanged={this.rowHasChanged.bind(this)}
+        // renderDay={renderDay}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
 export default Calendar;
