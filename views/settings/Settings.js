65e90b161db8ce45fb63fadd8cb7301249ea2666
maizerocom <41408745+maizerocom@users.noreply.github.com>
2020-10-30 23:14:12 +0700

    Update Settings.js

--- views/settings/Settings.js
+++ views/settings/Settings.js
@@ -1,13 +1,87 @@
-import React from "react";
-import { Button } from "react-native";
+import React, { useState } from "react";
+import { Button, Text, View, Switch, StyleSheet } from "react-native";
 
 const Settings = (props) => {
+  const [isLinkAccount, setisLinkAccount] = useState(true);
+  const [isTimeCheckin, setTimeCheckin] = useState(false);
+  const [isCalendar, setCalendar] = useState(false);
   return (
-    <Button
-      title="Account"
-      onPress={() => props.navigation.navigate("Account")}
-    />
+    <View style={styles.screen}>
+      <Text style={styles.title}>Account</Text>
+
+      <View style={styles.container}>
+        <Text>
+          Link to account
+          </Text>
+        <Switch
+          value={isLinkAccount}
+          onValueChange={value => setisLinkAccount(value)}
+          trackColor={{ true: "#88c943", false: "#ff3b21" }}
+          thumbColor="orange"
+        />
+      </View>
+
+      <Text style={styles.title}>Notification</Text>
+
+      <View style={styles.container}>
+        <Text>
+          Time Check-in (arrival, absent, departure)
+        </Text>
+        <Switch
+          value={isTimeCheckin}
+          onValueChange={value => setTimeCheckin(value)}
+          trackColor={{ true: "#88c943", false: "#ff3b21" }}
+          thumbColor="orange"
+        />
+      </View>
+
+      <View style={styles.container}>
+        <Text>
+          Calendar (event)
+        </Text>
+        <Switch
+          value={isCalendar}
+          onValueChange={value => setCalendar(value)}
+          trackColor={{ true: "#88c943", false: "#ff3b21" }}
+          thumbColor="orange"
+        />
+      </View>
+    <View style={styles.button}>
+      <Button
+        title="Account"
+        onPress={() => props.navigation.navigate("Account")}
+      />
+      </View>
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: "5%",
+    backgroundColor: "#e7eaef",
+    // alignItems: "center",
+  },
+  container: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "100%",
+    marginVertical: 5,
+    backgroundColor: "#ffffff",
+    padding: 10
+  },
+  title: {
+    fontSize: 18,
+    textAlign: "left",
+    fontWeight: "bold",
+    marginVertical: 5,
+  },
+  button: {
+    marginVertical: 20,
+    paddingRight: "50%"
+  }
+});
 
 export default Settings;
