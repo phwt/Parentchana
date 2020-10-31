008106464cc07deeb0f5da697639dbfb4811a7a0
phwt.smwt <phwt.smwt@gmail.com>
2020-10-31 14:35:30 +0700

    Simple QR scanner layout

--- views/pickup/Pickup.js
+++ views/pickup/Pickup.js
@@ -1,8 +1,49 @@
-import React from "react";
-import { Text } from "react-native";
+import React, { useState, useEffect } from "react";
+import { Text, StyleSheet, Button } from "react-native";
+import { Col, Row, Grid } from "react-native-easy-grid";
+import { BarCodeScanner } from "expo-barcode-scanner";
 
 const Pickup = () => {
-  return <Text>Pickup</Text>;
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+  };
+
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
+  return (
+    <Grid>
+      <Row size={75}>
+        <BarCodeScanner
+          onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+          style={StyleSheet.absoluteFillObject}
+        />
+      </Row>
+      <Row size={25}>
+        {scanned && (
+          <Button
+            title={"Tap to Scan Again"}
+            onPress={() => setScanned(false)}
+          />
+        )}
+      </Row>
+    </Grid>
+  );
 };
 
 export default Pickup;
