cc0256df0cca699175a2a88ab672ea3f8b088c9f
maizerocom <41408745+maizerocom@users.noreply.github.com>
2020-11-01 23:50:56 +0700

    Update CheckInDetailed :tada:

--- views/checkin/CheckIn.js
+++ views/checkin/CheckIn.js
@@ -1,8 +1,33 @@
-import React from "react";
-import { Text } from "react-native";
+import React, { useState } from "react";
+import { Text, View, ScrollView, StyleSheet } from "react-native";
+import { checkinAPI } from "../../store/mockData";
+import { color } from "react-native-reanimated";
+import { check } from "prettier";
+
 
 const CheckIn = () => {
-  return <Text>Time Check-in</Text>;
+  let [checkinList, setcheckinList] = useState(checkinAPI);
+  // console.log(checkinList);
+  return <Text>CheckInScreen</Text>
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginHorizontal: 10,
+    marginTop: 10
+  },
+  scrollView: {
+    backgroundColor: 'pink',
+    marginHorizontal: 20,
+  },
+  text: {
+    fontSize: 25,
+  },
+  title: {
+    fontWeight: "bold",
+  }
+});
+
 
 export default CheckIn;
