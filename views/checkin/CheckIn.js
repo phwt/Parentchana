3aa6ce25fa376854af5208afaa3ac81431ecf09c
Tummasorn Tantiyapinun <42957781+tangtms@users.noreply.github.com>
2020-11-02 23:21:53 +0700

    Update CheckIn.js

--- views/checkin/CheckIn.js
+++ views/checkin/CheckIn.js
@@ -1,11 +1,62 @@
 import React, { useState } from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View } from "react-native";
+import {CalendarList} from 'react-native-calendars';
 import { checkinAPI } from "../../store/mockData";
 
 const CheckIn = () => {
-  let [checkinList, setcheckinList] = useState(checkinAPI);
-  // console.log(checkinList);
-  return <Text>CheckInScreen</Text>;
+  const [checkinList, setcheckinList] = useState(checkinAPI);
+
+  return (
+    <CalendarList
+      // testID={testIDs.calendarList.CONTAINER}
+      current={'2020-06-10'}
+      pastScrollRange={24}
+      futureScrollRange={24}
+      renderHeader={(date) => {
+        const header = date.toString('MMMM yyyy');
+        const [month, year] = header.split(' ');
+        const textStyle = {
+          fontSize: 18,
+          fontWeight: 'bold',
+          paddingTop: 10,
+          paddingBottom: 10,
+          color: '#5E60CE',
+          paddingRight: 5
+        };
+
+        return(
+          <View style={{
+            flexDirection: 'row',
+            width: '100%',
+            justifyContent: 'space-between',
+            marginTop: 10,
+            marginBottom: 10
+          }}>
+            <Text style={{marginLeft: 5, ...textStyle}}>{`${month}`}</Text>
+            <Text style={{marginRight: 5, ...textStyle}}>{year}</Text>
+          </View>
+        );
+      }}
+      theme={{
+        'stylesheet.calendar.header': {
+          dayHeader: {
+            fontWeight: '600',
+            color: '#48BFE3'
+          }
+        },
+        'stylesheet.day.basic': {
+          today: {
+            borderColor: '#48BFE3',
+            borderWidth: 0.8
+          },
+          todayText: {
+            color: '#5390D9',
+            fontWeight: '800'
+          }
+        }
+      }}
+    />);
+
 };
 
 const styles = StyleSheet.create({
