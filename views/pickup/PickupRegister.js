cb5ed6298ea2163a8cb222d494c9187ae94149ec
phwt.smwt <phwt.smwt@gmail.com>
2020-11-02 21:57:39 +0700

    Simple student register

--- views/pickup/PickupRegister.js
+++ views/pickup/PickupRegister.js
@@ -1,8 +1,39 @@
-import React from "react";
-import { Text } from "react-native";
+import React, { useState } from "react";
+import { View, Text, TextInput, Button } from "react-native";
 
 const PickupRegister = () => {
-  return <Text>Pickup Register</Text>;
+  const [registerInput, setRegisterInput] = useState("");
+  const [registerList, setRegisterList] = useState([]);
+
+  const addStudent = () => {
+    if (!registerList.includes(registerInput)) {
+      setRegisterList([...registerList, registerInput]);
+      setRegisterInput("");
+    } else {
+      alert("Student already exist!");
+    }
+  };
+
+  const removeStudent = (id) => {
+    setRegisterList(registerList.filter((i) => i !== id));
+  };
+
+  return (
+    <View>
+      <TextInput
+        style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
+        onChangeText={setRegisterInput}
+        value={registerInput}
+      />
+      <Button title="Add" onPress={addStudent} />
+      {registerList.map((i) => (
+        <>
+          <Text style={{ textAlign: "center" }}>{i}</Text>
+          <Button title="Remove" onPress={() => removeStudent(i)} />
+        </>
+      ))}
+    </View>
+  );
 };
 
 export default PickupRegister;
