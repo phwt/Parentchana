e71a0d6780d8c02108f21c9a7dabead0b0a20413
Tummasorn Tantiyapinun <42957781+tangtms@users.noreply.github.com>
2020-11-02 23:15:41 +0700

    Convert calendarAPI object

--- views/calendar/Calendar.js
+++ views/calendar/Calendar.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import { Agenda } from "react-native-calendars";
+import { calendarAPI } from "../../store/mockData";
 
 const timeToString = (time) => {
   const date = new Date(time);
@@ -8,42 +9,69 @@ const timeToString = (time) => {
 };
 
 const Calendar = () => {
-  const [items, setItems] = useState({});
-  const loadItems = (day) => {
-    setTimeout(() => {
-      for (let i = -15; i < 85; i++) {
-        const time = day.timestamp + i * 24 * 60 * 60 * 1000;
-        const strTime = timeToString(time);
-        if (!items[strTime]) {
-          items[strTime] = [];
-          const numItems = Math.floor(Math.random() * 3 + 1);
-          for (let j = 0; j < numItems; j++) {
-            items[strTime].push({
-              name: "Item for " + strTime + " #" + j,
-              height: Math.max(50, Math.floor(Math.random() * 150)),
-            });
-          }
-        }
-      }
-      const newItems = {};
-      Object.keys(items).forEach((key) => {
-        newItems[key] = items[key];
-      });
-      setItems(newItems);
-    }, 1000);
-  };
+  // const [calendarAPI, setcalendarAPI] = useState(calendarAPI);
+  // const calendarAPI = {
+  //   "2012-05-21": [{ name: "test 1" }],
+  //   "2012-05-23": [{ name: "cat" }],
+  //   "2012-05-24": [],
+  //   "2012-05-25": [{ name: "dog" }, { name: "yo" }],
+  // };
+  // const [items, setItems] = useState(calendarAPI);
+  // console.log(calendarAPI[0].summary);
+  var calendarData = {};
+  for (var i = 0; i < Object.keys(calendarAPI).length; i++) {
+    //if key doesnt exist
+    if (calendarAPI[i].start.date.toString() in calendarAPI){
+      calendarData[calendarAPI[i].start.date.toString()].push({ name: calendarAPI[i].summary });
+    }
+    else{
+      calendarData[calendarAPI[i].start.date.toString()] = [{ name: calendarAPI[i].summary }];
+    }
+    //elseif key already exist
+  }
+  console.log(calendarData);
 
+  // const loadItems = (day) => {
+  //   setTimeout(() => {
+  //     for (let i = -15; i < 85; i++) {
+  //       const time = day.timestamp + i * 24 * 60 * 60 * 1000;
+  //       const strTime = timeToString(time);
+  //       if (!items[strTime]) {
+  //         items[strTime] = [];
+  //         const numItems = Math.floor(Math.random() * 3 + 1);
+  //         for (let j = 0; j < numItems; j++) {
+  //           items[strTime].push({
+  //             name: "Item for " + strTime + " #" + j,
+  //             height: Math.max(50, Math.floor(Math.random() * 150)),
+  //           });
+  //         }
+  //       }
+  //     }
+  //     const newItems = {};
+  //     Object.keys(items).forEach((key) => {
+  //       newItems[key] = items[key];
+  //     });
+  //     setItems(newItems);
+  //   }, 1000);
+  // };
+  const renderItem = (item) => {
+    return (
+      <TouchableOpacity style={[styles.item, { height: 80 }]}>
+        <Text>{item.name}</Text>
+      </TouchableOpacity>
+    );
+  };
   return (
     <View style={{ flex: 1 }}>
       <Agenda
         // testID={testIDs.agenda.CONTAINER}
-        items={items}
-        loadItemsForMonth={loadItems}
-        selected={"2020-10-30"}
-        // renderItem={this.renderItem.bind(this)}
+        items={calendarData}
+        // loadItemsForMonth={loadItems}
+        selected={"2020-11-06"}
+        renderItem={renderItem}
+        // renderEmptyDate={() => {return (<View />);}}
         // renderEmptyDate={this.renderEmptyDate.bind(this)}
         // rowHasChanged={this.rowHasChanged.bind(this)}
-        // renderDay={renderDay}
       />
     </View>
   );
@@ -55,6 +83,19 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  item: {
+    backgroundColor: "white",
+    flex: 1,
+    borderRadius: 5,
+    padding: 10,
+    marginRight: 10,
+    marginTop: 17,
+  },
+  emptyDate: {
+    height: 15,
+    flex: 1,
+    paddingTop: 30,
   },
 });
 export default Calendar;
