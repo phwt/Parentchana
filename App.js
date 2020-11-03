289f2354a24621d0958f300800a8a8c1aa890cc0
phwt.smwt <phwt.smwt@gmail.com>
2020-11-03 20:39:38 +0700

    Theming configuration

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
-// import { StatusBar } from "expo-status-bar";
 import React from "react";
-// import { StyleSheet, Text, View } from "react-native";
 import Navigator from "./modules/Navigator";
+import { DefaultTheme, Provider as PaperProvider } from "react-native-paper";
+import Color from "./modules/Color";
 
 import * as firebase from "firebase";
 import "firebase/firestore";
@@ -9,8 +9,25 @@ import { firebaseConfig } from "./config";
 
 if (!firebase.apps.length) firebase.initializeApp(firebaseConfig);
 
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: Color.primary,
+    accent: Color.secondary,
+    success: Color.success,
+    info: Color.info,
+    warning: Color.warning,
+    danger: Color.danger,
+  },
+};
+
 const App = () => {
-  return <Navigator />;
+  return (
+    <PaperProvider theme={theme}>
+      <Navigator />
+    </PaperProvider>
+  );
 };
 
 export default App;
---
+++ modules/Color.js
@@ -0,0 +1,22 @@
+export default {
+  white: "#fff",
+  gray100: "#f8f9fa",
+  gray200: "#e9ecef",
+  gray300: "#dee2e6",
+  gray400: "#ced4da",
+  gray500: "#adb5bd",
+  gray600: "#6c757d",
+  gray700: "#495057",
+  gray800: "#343a40",
+  gray900: "#212529",
+  black: "#000",
+
+  primary: "#5179d6",
+  secondary: "#6c757d",
+  success: "#30c78d",
+  info: "#5cace5",
+  warning: "#e5ae67",
+  danger: "#e56767",
+  light: () => this.gray100,
+  dark: () => this.gray800,
+};
