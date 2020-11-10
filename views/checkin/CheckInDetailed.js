70d2bfbab769919dd755031e33def9ba76f2c7ba
phwt.smwt <phwt.smwt@gmail.com>
2020-11-10 23:44:53 +0700

    Render using DataTable

--- views/checkin/CheckInDetailed.js
+++ views/checkin/CheckInDetailed.js
@@ -1,14 +1,9 @@
 import React, { useEffect, useState } from "react";
-import {
-  Text,
-  View,
-  StyleSheet,
-  SafeAreaView,
-  FlatList,
-} from "react-native";
+import { Text, View, StyleSheet, SafeAreaView, FlatList } from "react-native";
 import moment from "moment";
 import { connect } from "react-redux";
 import { loadCheckinList } from "../../store/actions/checkinActions";
+import { DataTable } from "react-native-paper";
 
 const CheckInDetailed = (props) => {
   const [selectedRange, setSelectedRange] = useState(new Date("01/11/2020")); // TODO: Handle month selection
@@ -38,10 +33,8 @@ const CheckInDetailed = (props) => {
   const ColorDot = ({ ontime }) => (
     <View
       style={{
-        width: "10%",
-        height: "50%",
-        marginTop: "7%",
-        marginRight: "5%",
+        width: 8,
+        height: 8,
         borderRadius: 50,
         backgroundColor: ontime ? "green" : "orange",
       }}
@@ -49,59 +42,32 @@ const CheckInDetailed = (props) => {
   );
 
   const renderItem = ({ item }) => {
-    const currentItem = checkinTable[item]
+    const currentItem = checkinTable[item];
     return (
-      <View
-        style={{
-          marginHorizontal: 10,
-          marginVertical: 5,
-        }}
-      >
-        <View
-          style={{
-            flexDirection: "row",
-            justifyContent: "space-between",
-          }}
-        >
-          <View style={styles.space}>
-            <Text style={styles.data}>{item}</Text>
-          </View>
-          <View style={styles.space}>
-            <ColorDot ontime={currentItem.arrival.ontime} />
-            <Text style={styles.data}>
-              {moment(currentItem.arrival.timestamp).format("HH:mm")}
-            </Text>
-          </View>
-          <View style={styles.space}>
-            <ColorDot ontime={currentItem.departure.ontime} />
-            <Text style={styles.data}>
-              {moment(currentItem.departure.timestamp).format("HH:mm")}
-            </Text>
-          </View>
-        </View>
-      </View>
+      <DataTable.Row>
+        <DataTable.Cell>{moment(item).format("DD/MM/YY")}</DataTable.Cell>
+        <DataTable.Cell>
+          <ColorDot ontime={currentItem.arrival.ontime} />
+          {moment(currentItem.arrival.timestamp).format("HH:mm")}
+        </DataTable.Cell>
+        <DataTable.Cell>
+          <ColorDot ontime={currentItem.departure.ontime} />
+          {moment(currentItem.departure.timestamp).format("HH:mm")}
+        </DataTable.Cell>
+      </DataTable.Row>
     );
   };
 
   return (
     <SafeAreaView style={styles.container}>
-      <View
-        style={{
-          margin: 10,
-          flexDirection: "row",
-        }}
-      >
-        <View style={styles.space}>
-          <Text style={styles.title}>Date</Text>
-        </View>
-        <View style={styles.space}>
-          <Text style={styles.title}>Arrival</Text>
-        </View>
-        <View style={styles.space}>
-          <Text style={styles.title}>Departure</Text>
-        </View>
-      </View>
-
+      <DataTable>
+        <DataTable.Header>
+          <DataTable.Title>Date</DataTable.Title>
+          <DataTable.Title>Arrival</DataTable.Title>
+          <DataTable.Title>Departure</DataTable.Title>
+        </DataTable.Header>
+      </DataTable>
+      
       <FlatList
         data={Object.keys(checkinTable)}
         renderItem={renderItem}
