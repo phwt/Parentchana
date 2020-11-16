92c85fe81ef934da521d641570ac0e729159d177
phwt.smwt <phwt.smwt@gmail.com>
2020-11-16 19:25:27 +0700

    Remove pagination and switch

--- views/calendar/CalendarFavorite.js
+++ views/calendar/CalendarFavorite.js
@@ -1,7 +1,6 @@
 import React, { useCallback, useEffect, useState } from "react";
-import { FlatList } from "react-native-gesture-handler";
+import { FlatList } from "react-native";
 import { DataTable } from "react-native-paper";
-import { Switch } from "react-native-paper";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchCalendarEvents } from "../../store/actions/calendarActions";
 
@@ -32,19 +31,12 @@ const CalendarFavorite = (props) => {
     );
   }, [favoriteEvents, events]);
 
-  const [isSwitchOn, setIsSwitchOn] = React.useState(false);
-
-  const onToggleSwitch = () => setIsSwitchOn(!isSwitchOn);
-
   const renderFavoriteItem = (itemData) => {
     console.log(itemData);
     return (
       <DataTable.Row>
         <DataTable.Cell>{itemData.item.start.date.toString()}</DataTable.Cell>
         <DataTable.Cell>{itemData.item.summary}</DataTable.Cell>
-        <DataTable.Cell>
-          <Switch value={isSwitchOn} onValueChange={onToggleSwitch} />
-        </DataTable.Cell>
       </DataTable.Row>
     );
   };
@@ -55,21 +47,11 @@ const CalendarFavorite = (props) => {
         <DataTable.Header>
           <DataTable.Title>Date</DataTable.Title>
           <DataTable.Title>Name</DataTable.Title>
-          <DataTable.Title>Alert</DataTable.Title>
         </DataTable.Header>
         <FlatList
           style={{ width: "100%" }}
           data={favoriteList}
           renderItem={renderFavoriteItem}
-        />
-
-        <DataTable.Pagination
-          page={1}
-          numberOfPages={3}
-          onPageChange={(page) => {
-            console.log(page);
-          }}
-          label="1-2 of 6"
         />
       </DataTable>
     </>
