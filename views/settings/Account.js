05fb03662b83e06f0a569ac782d9704f4b9a7199
maizerocom <41408745+maizerocom@users.noreply.github.com>
2020-11-18 22:47:43 +0700

    Update Icon Account (Profile) :tada:

--- views/settings/Account.js
+++ views/settings/Account.js
@@ -5,6 +5,7 @@ import * as firebase from "firebase";
 import { useSelector } from 'react-redux'
 import { Col, Row, Grid } from "react-native-easy-grid";
 import { Avatar } from 'react-native-paper';
+import Icon from 'react-native-vector-icons/Ionicons';
 // import { Icon } from 'react-native-elements'
 
 const Account = (props) => {
@@ -20,6 +21,7 @@ const Account = (props) => {
   };
 
   const currentUser = useSelector(state => state.profile)
+  console.log(currentUser.phoneNumber);
   return (
     <>
       <Grid>
@@ -35,21 +37,31 @@ const Account = (props) => {
         <Row size={10} style={{ justifyContent: 'center', backgroundColor: "grey" }}>
           <View style={{ justifyContent: 'center', }}>
             <View>
-              <Text style={{fontSize: 30}}>{currentUser.displayName}</Text>
+              <Text style={{ fontSize: 30 }}>{currentUser.displayName}</Text>
             </View>
           </View>
         </Row>
 
         <Row size={10} style={{ justifyContent: 'center', backgroundColor: "lightgrey" }}>
-          <View style={{ justifyContent: 'center', alignItems: 'center', }}>
+        <Col size={30} style={styles.colStyle}>
+            <View style={{ justifyContent: 'center', alignItems: 'flex-end', }}>
+              <Icon name="md-mail" size={32} color="black" />
+            </View>
+          </Col>
+          <Col size={60} style={{...styles.colStyle, marginLeft:20}}>
             <Text>Email{"\n"}{currentUser.email}</Text>
-          </View>
+          </Col>
         </Row>
-        
+
         <Row size={10} style={{ justifyContent: 'center', backgroundColor: "grey" }}>
-          <View style={{ justifyContent: 'center', alignItems: 'center', }}>
-            <Text >Email{"\n"}{currentUser.email}</Text>
-          </View>
+          <Col size={30} style={styles.colStyle}>
+            <View style={{ justifyContent: 'center', alignItems: 'flex-end', }}>
+              <Icon name="md-call" size={32} color="black" />
+            </View>
+          </Col>
+          <Col size={60} style={{...styles.colStyle, marginLeft:20}}>
+            <Text>Phone{"\n"}{currentUser.phoneNumber == null ? "08X-XXX-XXXX(null)": currentUser.phoneNumber}</Text>
+          </Col>
         </Row>
 
         <Row size={40} style={{ justifyContent: 'center', backgroundColor: "lightgrey" }}>
@@ -87,6 +99,9 @@ const styles = StyleSheet.create({
     color: "black",
     paddingRight: 5,
   },
+  colStyle: {
+    justifyContent: 'center',
+  }
 });
 
 export default Account;
