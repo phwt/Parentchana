f6b2b055ce180594e5023fbc1e1afb03427559cb
phwt.smwt <phwt.smwt@gmail.com>
2020-11-19 22:19:44 +0700

    Hide settings based on roles

--- components/settings/NotificationSection.js
+++ components/settings/NotificationSection.js
@@ -2,14 +2,21 @@ import React from "react";
 import { Divider, List } from "react-native-paper";
 import NotificationCalendarItem from "./NotificationCalendarItem";
 import NotificationCheckinItem from "./NotificationCheckinItem";
+import { useSelector } from "react-redux";
 
 const NotificationSection = () => {
+  const role = useSelector((state) => state.auth.role);
+
   return (
     <>
       <List.Section>
         <List.Subheader>Notification</List.Subheader>
-        <Divider />
-        <NotificationCheckinItem />
+        {role === 1 && (
+          <>
+            <Divider />
+            <NotificationCheckinItem />
+          </>
+        )}
         <Divider />
         <NotificationCalendarItem />
         <Divider />
--- views/settings/Settings.js
+++ views/settings/Settings.js
@@ -2,13 +2,20 @@ import React from "react";
 import PlateSection from "../../components/settings/PlateSection";
 import StudentSection from "../../components/settings/StudentSection";
 import NotificationSection from "../../components/settings/NotificationSection";
+import { useSelector } from "react-redux";
 
 const Settings = () => {
+  const role = useSelector((state) => state.auth.role);
+
   return (
     <>
       <NotificationSection />
-      <PlateSection />
-      <StudentSection />
+      {role === 1 && (
+        <>
+          <PlateSection />
+          <StudentSection />
+        </>
+      )}
     </>
   );
 };
