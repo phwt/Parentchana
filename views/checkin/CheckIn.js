1c4a81840756be1225bc4460e482b9f1627295bd
phwt.smwt <phwt.smwt@gmail.com>
2020-11-19 11:04:00 +0700

    Remove grid usage

--- views/checkin/CheckIn.js
+++ views/checkin/CheckIn.js
@@ -9,7 +9,6 @@ import {
 import { Calendar } from "react-native-calendars";
 import moment from "moment";
 import { useSelector } from "react-redux";
-import { Row, Grid } from "react-native-easy-grid";
 import CheckInTable from "../../components/checkin/CheckInTable";
 import { computeMarkedDates } from "../../modules/CheckinUtils";
 
@@ -21,64 +20,55 @@ const CheckIn = ({ navigation }) => {
   const checkinList = useSelector((state) => state.checkin.list);
 
   return (
-    <Grid>
-      <Row size={55}>
-        <Calendar
-          markedDates={computeMarkedDates(checkinList)}
-          markingType={"period"}
-          current={moment().utcOffset("+05:30").format("YYYY-MM-DD")}
-          renderHeader={(date) => {
-            const header = date.toString("MMMM yyyy");
-            const [month, year] = header.split(" ");
+    <>
+      <Calendar
+        markedDates={computeMarkedDates(checkinList)}
+        markingType={"period"}
+        current={moment().utcOffset("+05:30").format("YYYY-MM-DD")}
+        renderHeader={(date) => {
+          const header = date.toString("MMMM yyyy");
+          const [month, year] = header.split(" ");
 
-            return (
-              <View
-                style={{
-                  flexDirection: "row",
-                  width: "77%",
-                  justifyContent: "center",
-                  marginTop: 10,
-                  marginBottom: 10,
-                }}
-              >
-                <Text
-                  style={{ marginLeft: 75, ...styles.textStyle }}
-                >{`${month}`}</Text>
-                <Text style={{ marginRight: 0, ...styles.textStyle }}>
-                  {year}
-                </Text>
-              </View>
-            );
+          return (
+            <View
+              style={{
+                flexDirection: "row",
+                width: "77%",
+                justifyContent: "center",
+                marginTop: 10,
+                marginBottom: 10,
+              }}
+            >
+              <Text
+                style={{ marginLeft: 75, ...styles.textStyle }}
+              >{`${month}`}</Text>
+              <Text style={{ marginRight: 0, ...styles.textStyle }}>
+                {year}
+              </Text>
+            </View>
+          );
+        }}
+      />
+      <SafeAreaView style={styles.container}>
+        <TouchableOpacity
+          onPress={() => {
+            navigation.navigate("CheckInDetailed");
           }}
-        />
-      </Row>
-      <Row size={45}>
-        <SafeAreaView style={styles.container}>
-          <TouchableOpacity
-            onPress={() => {
-              navigation.navigate("CheckInDetailed");
-            }}
-          >
-            <CheckInTable
-              selectedRange={selectedRange}
-              checkinList={checkinList}
-            />
-          </TouchableOpacity>
-        </SafeAreaView>
-      </Row>
-    </Grid>
+        >
+          <CheckInTable
+            selectedRange={selectedRange}
+            checkinList={checkinList}
+          />
+        </TouchableOpacity>
+      </SafeAreaView>
+    </>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginHorizontal: 10,
-    marginTop: 10,
-  },
-  scrollView: {
-    backgroundColor: "pink",
-    marginHorizontal: 20,
+    margin: 10,
   },
   text: {
     fontSize: 25,
@@ -89,10 +79,10 @@ const styles = StyleSheet.create({
   textStyle: {
     fontSize: 20,
     // fontWeight: 'bold',
-    paddingTop: 10,
-    paddingBottom: 10,
+    // paddingTop: 10,
+    // paddingBottom: 10,
     color: "black",
-    paddingRight: 5,
+    // paddingRight: 5,
   },
 });
 
