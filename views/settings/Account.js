9d45041ada99c8c9898153be8c4731e0b4e1f83f
Tummasorn Tantiyapinun <42957781+tangtms@users.noreply.github.com>
2020-11-20 11:26:07 +0700

    Update Account.js

--- views/settings/Account.js
+++ views/settings/Account.js
@@ -2,10 +2,10 @@ import React from "react";
 import { Text, StyleSheet, View, Image } from "react-native";
 import { Button } from "react-native-paper";
 import * as firebase from "firebase";
-import { useSelector } from 'react-redux'
+import { useSelector } from "react-redux";
 import { Col, Row, Grid } from "react-native-easy-grid";
-import { Avatar } from 'react-native-paper';
-import Icon from 'react-native-vector-icons/Ionicons';
+import { Avatar } from "react-native-paper";
+import Icon from "react-native-vector-icons/Ionicons";
 // import { Icon } from 'react-native-elements'
 
 const Account = (props) => {
@@ -20,51 +20,84 @@ const Account = (props) => {
     }
   };
 
-  const currentUser = useSelector(state => state.profile)
+  const currentUser = useSelector((state) => state.profile);
   console.log(currentUser.phoneNumber);
   return (
     <>
       <Grid>
-        <Row size={30} style={{ justifyContent: 'center', backgroundColor: "grey" }}>
-          <View style={{ justifyContent: 'flex-end', }}>
+        <Row
+          size={30}
+          style={{ justifyContent: "center", backgroundColor: "white" }}
+        >
+          <View style={{ justifyContent: "flex-end" }}>
             <View>
-              <Avatar.Image size={100} source={{ uri: "https://pbs.twimg.com/profile_images/1181451487353769985/XtN0ke9p_400x400.jpg" }} />
+              <Avatar.Image
+                size={100}
+                source={{
+                  uri: "https://www.w3schools.com/howto/img_avatar.png",
+                }}
+              />
               {/* <Text>{currentUser.photoURL}</Text> */}
             </View>
           </View>
         </Row>
 
-        <Row size={10} style={{ justifyContent: 'center', backgroundColor: "grey" }}>
-          <View style={{ justifyContent: 'center', }}>
+        <Row
+          size={5}
+          style={{
+            justifyContent: "center",
+            backgroundColor: "white",
+            paddingBottom: 20,
+          }}
+        >
+          <View style={{ justifyContent: "center" }}>
             <View>
               <Text style={{ fontSize: 30 }}>{currentUser.displayName}</Text>
+              <Text style={{ fontSize: 30 }}>Tummasorn T</Text>
             </View>
           </View>
         </Row>
 
-        <Row size={10} style={{ justifyContent: 'center', backgroundColor: "lightgrey" }}>
-        <Col size={30} style={styles.colStyle}>
-            <View style={{ justifyContent: 'center', alignItems: 'flex-end', }}>
-              <Icon name="md-mail" size={32} color="black" />
+        <Row
+          size={5}
+          style={{ justifyContent: "center", backgroundColor: "white" }}
+        >
+          <Col size={35} style={styles.colStyle}>
+            <View style={{ justifyContent: "center", alignItems: "flex-end" }}>
+              <Icon name="md-mail" size={15} color="black" />
             </View>
           </Col>
-          <Col size={60} style={{...styles.colStyle, marginLeft:20}}>
-            <Text>Email{"\n"}{currentUser.email}</Text>
+          <Col size={60} style={{ ...styles.colStyle, marginLeft: 20 }}>
+            <Text>
+              {/* Email{"\n"} */}
+              {currentUser.email}
+            </Text>
           </Col>
         </Row>
 
-        <Row size={10} style={{ justifyContent: 'center', backgroundColor: "grey" }}>
-          <Col size={30} style={styles.colStyle}>
-            <View style={{ justifyContent: 'center', alignItems: 'flex-end', }}>
-              <Icon name="md-call" size={32} color="black" />
+        <Row
+          size={5}
+          style={{ justifyContent: "center", backgroundColor: "white" }}
+        >
+          <Col size={35} style={styles.colStyle}>
+            <View style={{ justifyContent: "center", alignItems: "flex-end" }}>
+              <Icon name="md-call" size={15} color="black" />
             </View>
           </Col>
-          <Col size={60} style={{...styles.colStyle, marginLeft:20}}>
-            <Text>Phone{"\n"}{currentUser.phoneNumber == null ? "08X-XXX-XXXX(null)": currentUser.phoneNumber}</Text>
+          <Col size={60} style={{ ...styles.colStyle, marginLeft: 20 }}>
+            <Text>
+              {/* Phone{"\n"} */}
+              {currentUser.phoneNumber == null
+                ? "08X-XXX-XXXX(null)"
+                : currentUser.phoneNumber}
+            </Text>
           </Col>
         </Row>
 
-        <Row size={40} style={{ justifyContent: 'center', backgroundColor: "lightgrey" }}>
+        <Row
+          size={40}
+          style={{ justifyContent: "center", backgroundColor: "white" }}
+        >
           <View style={{ marginTop: 20 }}>
             <Button mode="contained" onPress={logout}>
               Logout
@@ -100,8 +133,8 @@ const styles = StyleSheet.create({
     paddingRight: 5,
   },
   colStyle: {
-    justifyContent: 'center',
-  }
+    justifyContent: "center",
+  },
 });
 
 export default Account;
