f8d04abc7e850e135aa069d710378fbedcba0151
phwt.smwt <phwt.smwt@gmail.com>
2020-12-03 23:55:09 +0700

    Create config-example.js

---
+++ config-example.js
@@ -0,0 +1,16 @@
+// Firebase Project Config
+export const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+};
+
+// Google Calendar API Config
+export const calendarConfig = {
+  calendarId: "",
+  apiKey: "",
+};
